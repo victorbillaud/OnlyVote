3634caaeacc07eaf863ef88b02d37e2f414041c3
Paul Guerre <pguerreftb@gmail.com>
2022-06-14 19:51:47 +0200

    Add android simple user interface test

---
+++ app/src/main/java/com/example/onlyvote/DisplayMessageActivity.kt
@@ -0,0 +1,18 @@
+package com.example.onlyvote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+    }
+}
