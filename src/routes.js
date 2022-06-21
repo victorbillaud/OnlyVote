b06159d5abec68917400562754b0dfb328f442a4
HugoDeSag <hugo.le.monies.de.sagazan@efrei.net>
2022-06-21 09:21:59 +0200

    ajoute routes candidat et resultat

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 const
     ROUTE_FORM = '/form'
     ROUTE_VOTE = '/vote'
+    ROUTE_DEPARTMENT = '/department'
+    ROUTE_CANDIDAT = '/candidat'
+    ROUTE_RESULTAT = '/resultat'
     ;
 
 module.exports = {
     ROUTE_FORM,
-    ROUTE_VOTE
+    ROUTE_VOTE,
+    ROUTE_DEPARTMENT,
+    ROUTE_CANDIDAT,
+    ROUTE_RESULTAT
 }
