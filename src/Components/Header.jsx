fd573e74ea9e41344ad278c37266a569c8f3f4ff
victorbillaud <victorbillaud.fr>
2022-06-29 17:33:47 +0200

    bouton header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,22 @@
 import logo_OV from '../Assets/logo.png';
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+
 
 function Header(){
+    const location = useLocation();
     return (
         <div className="header">
             <header className="App-header">
                 <Link to="/" class="logo" alt="logo_OnlyVote" >
                 <img src={logo_OV} class="logo" alt="logo_OnlyVote" />
                 </Link>
-                <Link to="/vote" class="button_vote">
-                    Voter
-                </Link>
+                {
+                    location.pathname !== "/vote" ? 
+                    <Link to="/vote" class="button_vote">
+                        Voter
+                    </Link> : null
+                }
+                {console.log(location.pathname)}
             </header>
         </div>
     );
