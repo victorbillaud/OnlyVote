76d412e4d25af663f9f058d6b997406a9c1ee6c2
Paul Guerre <pguerreftb@gmail.com>
2022-07-04 15:09:08 +0200

    Vote page first step added

--- app/src/main/java/com/example/onlyvote/ui/vote/VoteFragment.kt
+++ app/src/main/java/com/example/onlyvote/ui/vote/VoteFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.example.onlyvote.data.CandidateRequest
 import com.example.onlyvote.databinding.FragmentVoteBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.gson.Gson
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import android.widget.*
+import com.example.onlyvote.R
 
 class VoteFragment : Fragment() {
 
@@ -22,17 +29,49 @@ class VoteFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val voteViewModel =
-            ViewModelProvider(this).get(VoteViewModel::class.java)
-
         _binding = FragmentVoteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        voteViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        fetchCandidateNames().start()
+
+        binding.apply {
+            buttonSave.setOnClickListener {
+
+            }
         }
+
         return root
+    }
+
+    private fun fetchCandidateNames() : Thread {
+        return Thread {
+            val url = URL("https://onlyvote.victorbillaud.fr/candidat")
+            val connection = url.openConnection() as HttpURLConnection
+            val inputSystem = connection.inputStream
+            val inputStreamReader = InputStreamReader(inputSystem, "UTF-8")
+            val request: Array<CandidateRequest> = Gson().fromJson(inputStreamReader, Array<CandidateRequest>::class.java)
+
+            updateUiCandidateNamesSpinner(request)
+
+            inputStreamReader.close()
+            inputSystem.close()
+        }
+    }
+
+    private fun updateUiCandidateNamesSpinner(request: Array<CandidateRequest>) {
+        requireActivity().runOnUiThread {
+            kotlin.run {
+                val names: ArrayList<String> = ArrayList()
+
+                for (i in request.indices) {
+                    names.add(request.get(i).firstname + " " + request.get(i).lastname)
+                }
+
+                val adapter: ArrayAdapter<String> = ArrayAdapter(binding.root.context, android.R.layout.simple_list_item_1, names)
+
+                binding.spinnerCandidateNames.adapter = adapter
+            }
+        }
     }
 
     override fun onDestroyView() {
